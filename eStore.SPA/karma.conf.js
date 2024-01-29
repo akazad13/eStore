c4a16009f5fd6dc5de613620e43ea8e073565427
Md. Abul Kalam <akazad.cse13@gmail.com>
2024-01-29 19:55:24 +0600

    Updated angular project folder name

--- eStore.SPA/karma.conf.js
+++ eStore.SPA/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/shopizy'),
+      dir: require('path').join(__dirname, './coverage/eStore'),
       subdir: '.',
       reporters: [
         { type: 'html' },
